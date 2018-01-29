95fa8d9add0c72074a7474f58fadf83e951660a0
Sytnik Maksim <Sytnik.M.Yu@gmail.com>
2018-01-29 09:03:35 +0500

    Message All an To

--- app/src/main/java/com/firebase_info/firebaseandroidchat/MainActivity.java
+++ app/src/main/java/com/firebase_info/firebaseandroidchat/MainActivity.java
@@ -44,12 +44,190 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    // Иное
+    // 1. Пользовательский интерфейс (UI)
+    private FirebaseAuth mAuth;
+    private AuthStateListener mAuthListener;
+    FirebaseUser user = mAuth.getInstance().getCurrentUser();
 
+    private ListView mMessageListView;
+    private EditText mMessageEditText;
+    private Button mSendButton;
+    private MessageAdapter mMessageAdapter;
+
+
+    // 2. Firebase
+    private FirebaseDatabase mFirebaseDatabase;
+
+    private DatabaseReference mMessageDatabaseReference,mUserDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    private String test = "ТЕСТ";
+    String[] str_weather;
+    String[] str_bot;
+    public static String LOG_TAG = "my_log";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        // 3. Связываем UI
+        mMessageListView = (ListView) findViewById(R.id.messageListView);
+        mMessageEditText = (EditText) findViewById(R.id.messageEditText);
+        mSendButton = (Button) findViewById(R.id.sendButton);
+
+
+        // 4. Инициализируем Firebase
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMessageDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+
+        // 5. Создаем слушатель базы данных
+
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s){
+
+                    Message message = dataSnapshot.getValue(Message.class);
+                    if(message.getUser().equals(user.getEmail()) || message.getTo().equals(user.getEmail()) || message.getTo().equals("all")) {
+                        mMessageAdapter.add(message);
+                    }
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+
+            // 6. Устанавливаем слушатель базы данных
+            mMessageDatabaseReference.addChildEventListener(mChildEventListener);
+        }
+
+        // 7. Создаем лист где будем хранить сообщения
+        List<Message> messages = new ArrayList<>();
+
+        // 8. Создаем и устанавливаем Адаптер для сообщений
+        mMessageAdapter = new MessageAdapter(this, R.layout.item_message, messages);
+        mMessageListView.setAdapter(mMessageAdapter);
+
+        // 9. Устанавливаем слушатель клика на кнопку, создаем сообщение, отправляем сообщение в базу, удаляем текст
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mMessageEditText.getText().toString().startsWith("/to")){
+                    String[] str =mMessageEditText.getText().toString().split(" ");
+                mMessageDatabaseReference.push().setValue(new Message(mMessageEditText.getText().toString().replace("/to "+str[1]+" ",""),user.getEmail(),str[1]));
+                mMessageEditText.setText("");
+                }
+
+                else if(mMessageEditText.getText().toString().startsWith("/справка")){
+                    String info = "________________________\n" +
+                            "ИНФОРМАЦИЯ \n" +
+                            "________________________\n\n" +
+                            "Для отправки сообщения нажмите кнопку 'Send'\n" +
+                            "Для отправки личного сообщения введите '/to <email получателя> <Текст сообщения>'\n" +
+                            "Данное приложение было разработанно в ознакомительных целях\n" +
+                            "________________________";
+                    mMessageEditText.setText("");
+                }
+                else{
+                    mMessageDatabaseReference.push().setValue(new Message(mMessageEditText.getText().toString(),user.getEmail(),"all"));
+                    mMessageEditText.setText("");
+                    new GetTok().execute();
+                }
+            }
+        });
+
+        // * устанавливаем слушатель текста
+        mMessageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    mSendButton.setEnabled(true);
+                } else {
+                    mSendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
     }
-}
 
+    // 10. Удаляем слушатель базы данных
+    @Override
+    protected void onDestroy() {
+        if (mChildEventListener != null) {
+            mMessageDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+            super.onDestroy();
+        }
+    }
+
+
+    private class GetTok extends AsyncTask<Void, Void, String> {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // получаем данные с внешнего ресурса
+            try {
+                URL url = new URL("http://belindn.ddns.net/test.php?mytokken="+ refreshedToken);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                resultJson = buffer.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            // выводим целиком полученную json-строку
+            Log.d(LOG_TAG, strJson);
+        }
+    }
+}
